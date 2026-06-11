89fdaf6b762df2503eedf17186659e0286b59b18
tharunponugoti <ttharunponugoti@gmail.com>
2026-06-11 14:44:31 +0530

    Python Introduction

---
+++ Python/1.Core Python/2.Variables.py
@@ -0,0 +1,49 @@
+ ********* VARIABLES *********
+ 
+A variables is a name given to a memory in a program
+Variables are used to store data that can be referenced and manipulated during program execution.
+ A variable is essentially a name that is assigned to a value.
+
+Unlike Java and many other languages, Python variables do not require explicit declaration of type.
+Type of the variable is inferred based on the value assigned.
+ 
+
+
+name="THARUN"
+AGE=24
+price=99.9
+here 99.9 is value and price is a variable
+  
+  
+ ********* Memory *******
+ tharun is stored in the memory of name in python 
+ 
+ 23 is stored in the memory of age in python 
+ 
+ 
+x = 5
+name = "Alex"  
+print(x)
+print(name)
+
+Output
+5
+Alex
+
+Rules for Naming Variables
+To use variables effectively, we must follow Python’s naming rules:
+
+Variable names can only contain letters, digits and underscores (_).
+A variable name cannot start with a digit.
+Variable names are case-sensitive like myVar and myvar are different.
+Avoid using Keywords like if, else, for as variable names.
+
+ name = "THARUN"
+ AGE = 24
+ price = 99.9
+ 
+ print(type(name))
+ print(type(AGE))
+ print(type(price))
+ 
+  
