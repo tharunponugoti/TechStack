e6ad0fb1410a6a93d31c6acd2dceda892667eed8
tharunponugoti <ttharunponugoti@gmail.com>
2026-06-11 14:29:06 +0530

    Pytho Introduction

---
+++ Python/1.Core Python/1.Introduction to Python And It's History.py
@@ -0,0 +1,41 @@
+Python is a high-level programming language known for its simple and readable syntax. It has the following features:
+
+Allows writing programs with fewer lines of code, improving readability.
+Automatically detects variable types at runtime, eliminating the need for explicit declarations.
+Used in web development, data analysis, automation and many other fields.
+Supports object-oriented, functional and procedural programming styles.
+Dynamically typed and has automatic garbage collection.
+
+what is Python? 
+  ~ Python is Simple & easy 
+  ~ Free & Open Source
+  ~ High level Language 
+  ~ Developed by Guido Van Rassum
+  ~ Portable 
+  
+  
+Understanding Hello World Program
+# This is a comment. It will not be executed.
+print("Hello, World!")
+
+print() is a built-in Python function that instructs the computer to display text on the screen.
+"Hello, World!" is a string, which is a sequence of text. Strings are enclosed in quotes (either single ' or double ").
+The scope of code blocks is defined using Indentation (spaces or tabs). This is different from C, C++ and Java which use braces {} to create a block of code.
+Related Articles.
+
+
+our first Program
+Print("Hello World!")
+
+print is a predefined function
+
+print("THARUN")
+print("My Age is 24")
+print(24)
+
+
+
+
+
+
+
